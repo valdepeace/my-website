2e873aa7eca0ca29d2105da3f0b0492870c15c9a
Andres Carmona <valdepeace@gmail.com>
2025-11-15 13:52:08 +0100

    Fix: Change onBrokenLinks to warn for RSS feed compatibility

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,7 +27,7 @@ const config: Config = {
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
